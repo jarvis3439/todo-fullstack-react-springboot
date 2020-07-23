d09c9b9cd853018dfd78c76bf3419c6a28ade087
Harsh Pithadiya <47056249+jarvis3439@users.noreply.github.com>
2020-07-23 06:45:13 -0700

    Made Login component as controlled component

--- todo-react-frontend/src/Components/TODO/Login.js
+++ todo-react-frontend/src/Components/TODO/Login.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "jarvis",
+      password: "",
+    };
+  }
+
+  onChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
     return (
       <div>
-        Username : <input type="text" name="username" />
-        Password : <input type="password" name="password" />
+        Username :{" "}
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.onChange}
+        />
+        Password :{" "}
+        <input
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.onChange}
+        />
         <button>LOGIN</button>
       </div>
     );
