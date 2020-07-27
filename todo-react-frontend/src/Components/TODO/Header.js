1bb6fefcfb2e24c64126b416f7e1bd1f6ca3f302
Harsh Pithadiya <47056249+jarvis3439@users.noreply.github.com>
2020-07-26 23:12:06 -0700

    Styling for Header component with bootstrap

--- todo-react-frontend/src/Components/TODO/Header.js
+++ todo-react-frontend/src/Components/TODO/Header.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   render() {
     return (
       <div>
-        Header
-        <hr />
+        <header>
+          <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <ul className="navbar-nav">
+              <li>
+                <Link className="nav-link" to="/welcome/jarvis">
+                  HOME
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/todos">
+                  Todos
+                </Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav navbar-collapse justify-content-end">
+              <li>
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link className="nav-link" to="/logout">
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
       </div>
     );
   }
